0199e8a29fc9b4bfb7ef5e7b00c914ad6f388449
Natalia Palej <palejnatalia@gmail.com>
2023-09-21 12:10:05 +0100

    Created GET and POST php/html files for forms

---
+++ WK2/displayGet.php
@@ -0,0 +1,9 @@
+<?php
+#variables
+$first_name = $_GET['first_name'];
+$last_name = $_GET['last_name'];
+
+echo $first_name;
+echo $last_name;
+
+?>
---
+++ WK2/displayPost.php
@@ -0,0 +1,9 @@
+<?php
+#variables
+$first_name = $_POST('first_name');
+$last_name = $_POST('last_name');
+
+echo $first_name;
+echo $last_name;
+
+?>
