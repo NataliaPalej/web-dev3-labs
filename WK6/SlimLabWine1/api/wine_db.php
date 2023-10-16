2f8c130a938fa07d36fbe97fcd1c646a0e118559
Natalia <PalejNatalia@gmail.com>
2023-10-16 10:00:04 +0100

    Working with php, postman and mysql - exercise wines

---
+++ WK6/SlimLabWine1/api/wine_db.php
@@ -0,0 +1,46 @@
+<?php
+function getWines() {
+	if (isset($_GET['sort'])){
+		$col=$_GET['sort'];
+	}
+	else{
+		$col="name";
+	}
+	$query = "SELECT * FROM wine ORDER BY "."$col";
+	try {
+	global $db;
+		$wines = $db->query($query);  
+		$wine = $wines->fetchAll(PDO::FETCH_ASSOC);
+		header("Content-Type: application/json", true);
+		echo '{"wines": ' . json_encode($wine) . '}';
+	} catch(PDOException $e) {
+		echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+	}
+}
+
+function getWine($id) {
+	$query = "SELECT * FROM wine WHERE id = '$id'";
+    try {
+		global $db;
+		$wines = $db->query($query);  
+		$wine = $wines->fetch(PDO::FETCH_ASSOC);
+        header("Content-Type: application/json", true);
+        echo json_encode($wine);
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+}
+
+function findByName($name) {
+$query = "SELECT * FROM wine WHERE name LIKE ". '"%'.$name.'%"'." ORDER BY name";
+    try {
+		global $db;
+		$wines = $db->query($query);  
+		$wine = $wines->fetch(PDO::FETCH_ASSOC);
+        header("Content-Type: application/json", true);
+        echo json_encode($wine);
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+}
+?>
